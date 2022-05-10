b26896044598589c0863834edfb3b5095254070c
ksuares1 <kimberlyjsuares@gmail.com>
2022-05-09 20:59:25 -0400

    Added react

---
+++ cards/src/components/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomeComponent from './Pages/Home';
+
+const WelcomeComponent = ()=>{
+    return(
+        <main>
+            <HomeComponent/>
+        </main>
+    )
+}
+
+
+export default WelcomeComponent;
