b90434fd51004a34ee0aa5b372b82ea44f4c6823
ksuares1 <kimberlyjsuares@gmail.com>
2022-05-09 22:07:47 -0400

    created homepage

--- cards/src/components/Welcome.js
+++ cards/src/components/Welcome.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import HomeComponent from './Pages/Home';
+import Button from 'react-bootstrap/Button';
+
 
 const WelcomeComponent = ()=>{
     return(
         <main>
-            <HomeComponent/>
+               <main>
+  <div className="mb-2">
+    <Button variant="primary" size="lg">
+      Large button
+    </Button>{' '}
+    <Button variant="secondary" size="lg">
+      Large button
+    </Button>
+  </div>
+     </main>
         </main>
     )
 }
