90c5dc0764888d6cc0a9c19136a1470248217742
ksuares1 <kimberlyjsuares@gmail.com>
2022-05-12 17:18:08 -0400

    added 52 deck of cards to game page

--- cards/src/Pages/Game.js
+++ cards/src/Pages/Game.js
@@ -1,13 +1,20 @@
 // import Button from 'react-bootstrap/Button';
+import Cards from '../component/Cards/Cards.js';
 
 const Game= ()=>{
     return(
         <section class="game-begins">
-        <div class="game-bg">
-         <img class="game-bg" src={require("../assets/img/card-game-img.jpg")} alt="green-background"/> 
-
-        </div>
+            {/* <div class="game-bg">
+                <img class="game-bg" src={require("../assets/img/card-game-img.jpg")} alt="green-background" />
+            </div> */}
+            <div>
+            <Cards/>
+            </div>
         </section>
+        
+       
+        
+            
     )
 }
 
---
+++ cards/src/component/Cards/Cards.js
@@ -0,0 +1,13 @@
+
+
+let Cards =() =>{
+    return(
+        <div class="52-deck">
+        <div>
+        <img class="52-deck" src={require("../../assets/img/array-of-cards.jpg")} alt="52-deck-of-cards"/>
+    </div>
+    </div>
+    )
+}
+
+export default Cards;
