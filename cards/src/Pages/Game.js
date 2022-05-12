de492d65245b16cc92696b2c338ed57da792ecd8
ksuares1 <kimberlyjsuares@gmail.com>
2022-05-12 15:57:59 -0400

    added game bg

--- cards/src/Pages/Game.js
+++ cards/src/Pages/Game.js
@@ -3,7 +3,7 @@
 const Game= ()=>{
     return(
         <section class="game-begins">
-        <div>
+        <div class="game-bg">
          <img class="game-bg" src={require("../assets/img/card-game-img.jpg")} alt="green-background"/> 
 
         </div>
--- cards/src/Pages/Home.js
+++ cards/src/Pages/Home.js
@@ -7,21 +7,21 @@ const Home = ()=>{
           <div>
             <h1 id="welcome"> Welcome to Card Festival</h1>
         <br/>
-      <div className="mb-2">
       
-      <Button variant="secondary" size="lg">
-      Quit
-      </Button>
-      <br/>
+      
+     
     <div class="home-card">
    <img class="bg" src={require("../assets/img/array-of-cards.jpg")} alt="assortment-of-cards"/>
     <Button class="play-button" variant="info" size="lg"><Link to="/game">
        Play
        </Link>
       </Button>
+      <Button variant="secondary" size="lg">
+      Quit
+      </Button>
+      <br/>
     </div>
    </div>
-      </div>
         </div>
     )
 }
