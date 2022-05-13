905677bd8a0d050c7bbd60f5633765dd83f8bd91
ksuares1 <kimberlyjsuares@gmail.com>
2022-05-13 12:29:17 -0400

    added diamond and spades cards

--- cards/src/Pages/Game.js
+++ cards/src/Pages/Game.js
@@ -5,7 +5,6 @@ const Game= ()=>{
     return(
         <section class="game-begins">
              <div class="game-bg">
-                {/* <img class="game-bg" src={require("../assets/img/card-game-img.jpg")} alt="green-background" /> */}
                 <Cards/>
             </div> 
         </section>
