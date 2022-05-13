10bc8068f5690b6c0e1deb893c0e1f1077e42daf
ksuares1 <kimberlyjsuares@gmail.com>
2022-05-13 12:53:23 -0400

    moved nd cards to own file

---
+++ cards/src/component/Cards/ND.js
@@ -0,0 +1,154 @@
+
+const NotDealt =()=>{
+return(
+<main>
+<div class="ND">
+{/* ND- Cards */}
+{/*Clubs  */}
+<div class="suit-clubs">
+    <img class="suit-clubs-cards 2" src={require("../../assets/img/suit-of-clubs/Club-Suit-2.jpg")} alt="suit-of clubs" /> 
+</div>
+<div class="suit-clubs">
+    <img class="suit-clubs-cards 3" src={require("../../assets/img/suit-of-clubs/Club-Suit-3.jpg")} alt="suit-of clubs" /> 
+</div>
+<div class="suit-clubs">
+    <img class="suit-clubs-cards 4" src={require("../../assets/img/suit-of-clubs/Club-Suit-4.jpg")} alt="suit-of clubs" /> 
+</div>
+
+<div class="suit-clubs">
+    <img class="suit-clubs-cards 5" src={require("../../assets/img/suit-of-clubs/Club-Suit-5.jpg")} alt="suit-of clubs" /> 
+</div>
+<div class="suit-clubs">
+    <img class="suit-clubs-cards 6" src={require("../../assets/img/suit-of-clubs/Club-Suit-6.jpg")} alt="suit-of clubs" /> 
+</div>
+
+<div class="suit-clubs">
+    <img class="suit-clubs-cards 7" src={require("../../assets/img/suit-of-clubs/Club-Suit-7.jpg")} alt="suit-of clubs" /> 
+</div>
+<div class="suit-clubs">
+    <img class="suit-clubs-cards 8" src={require("../../assets/img/suit-of-clubs/Club-Suit-8.jpg")} alt="suit-of clubs" /> 
+</div>
+<div class="suit-clubs">
+    <img class="suit-clubs-cards 9" src={require("../../assets/img/suit-of-clubs/Club-Suit-9.jpg")} alt="suit-of clubs" /> 
+</div>
+<div class="suit-clubs">
+    <img class="suit-clubs-cards 10" src={require("../../assets/img/suit-of-clubs/Club-Suit-10.jpg")} alt="suit-of clubs" /> 
+</div>
+
+<div class="suit-clubs">
+    <img class="suit-clubs-cards A" src={require("../../assets/img/suit-of-clubs/Club-Suit-A.jpg")} alt="suit-of clubs" /> 
+</div>
+</div>
+
+<br/>
+{/* Hearts */}
+<div class="ND suit-hearts">
+<div class="suit-hearts">
+    <img class="suit-clubs-cards A" src={require("../../assets/img/suit-of-hearts/hearts-A.png")} alt="suit-of hearts" /> 
+</div>
+<div class="suit-hearts">
+    <img class="suit-clubs-cards 2" src={require("../../assets/img/suit-of-hearts/hearts-2.png")} alt="suit-of hearts" /> 
+</div>
+<div class="suit-hearts">
+    <img class="suit-clubs-cards 3" src={require("../../assets/img/suit-of-hearts/hearts-3.png")} alt="suit-of hearts" /> 
+</div>
+<div class="suit-hearts">
+    <img class="suit-clubs-cards 4" src={require("../../assets/img/suit-of-hearts/hearts-4.png")} alt="suit-of hearts" /> 
+</div>
+<div class="suit-hearts">
+    <img class="suit-clubs-cards 5" src={require("../../assets/img/suit-of-hearts/hearts-5.png")} alt="suit-of hearts" /> 
+</div>
+<div class="suit-hearts">
+    <img class="suit-clubs-cards 6" src={require("../../assets/img/suit-of-hearts/hearts-6.png")} alt="suit-of hearts" /> 
+</div>
+<div class="suit-hearts">
+    <img class="suit-clubs-cards 7" src={require("../../assets/img/suit-of-hearts/hearts-7.png")} alt="suit-of hearts" /> 
+</div>
+<div class="suit-hearts">
+    <img class="suit-clubs-cards 8" src={require("../../assets/img/suit-of-hearts/hearts-8.png")} alt="suit-of hearts" /> 
+</div>
+<div class="suit-hearts">
+    <img class="suit-clubs-cards 9" src={require("../../assets/img/suit-of-hearts/hearts-9.png")} alt="suit-of hearts" /> 
+</div>
+<div class="suit-hearts">
+    <img class="suit-clubs-cards 10" src={require("../../assets/img/suit-of-hearts/hearts-10.png")} alt="suit-of hearts" /> 
+</div>
+<div class="suit-hearts">
+    <img class="suit-clubs-cards A" src={require("../../assets/img/suit-of-hearts/hearts-A.png")} alt="suit-of-hearts" /> 
+</div>
+</div>
+<br/>
+{/* Diamonds */}
+
+<div class="ND suit-diamonds">
+    <div class="suit-diamonds">
+    <img class="suit-clubs-cards 2 " src={require("../../assets/img/suit-of-diamonds/diamond-2.jpg")} alt="suit-of-diamonds" /> 
+    </div>
+    <div class="suit-diamonds">
+    <img class="suit-clubs-cards 3 " src={require("../../assets/img/suit-of-diamonds/diamond-3.png")} alt="suit-of-diamonds" /> 
+    </div>
+    <div class="suit-diamonds">
+    <img class="suit-clubs-cards 4 " src={require("../../assets/img/suit-of-diamonds/diamond-4.png")} alt="suit-of-diamonds" /> 
+    </div>
+    <div class="suit-diamonds">
+    <img class="suit-clubs-cards 5" src={require("../../assets/img/suit-of-diamonds/diamond-5.png")} alt="suit-of-diamonds" /> 
+    </div>
+    <div class="suit-diamonds">
+    <img class="suit-clubs-cards 6" src={require("../../assets/img/suit-of-diamonds/diamond-6.png")} alt="suit-of-diamonds" /> 
+    </div>
+    <div class="suit-diamonds">
+    <img class="suit-clubs-cards 7" src={require("../../assets/img/suit-of-diamonds/diamond-7.png")} alt="suit-of-diamonds" /> 
+    </div>
+    <div class="suit-diamonds">
+    <img class="suit-clubs-cards 8" src={require("../../assets/img/suit-of-diamonds/diamond-8.png")} alt="suit-of-diamonds" /> 
+    </div>
+    <div class="suit-diamonds">
+    <img class="suit-clubs-cards 9" src={require("../../assets/img/suit-of-diamonds/diamond-9.png")} alt="suit-of-diamonds" /> 
+    </div>
+    <div class="suit-diamonds">
+    <img class="suit-clubs-cards 10" src={require("../../assets/img/suit-of-diamonds/diamond-10.png")} alt="suit-of-diamonds" /> 
+    </div>
+    <div class="suit-diamonds">
+    <img class="suit-clubs-cards A" src={require("../../assets/img/suit-of-diamonds/diamond-A.png")} alt="suit-of-diamonds" /> 
+    </div>
+</div>
+<br/>
+
+{/* Spades */}
+<div class="ND suit-spades">
+<div class="suit-spades">
+    <img class="suit-clubs-cards 2" src={require("../../assets/img/suit-of-spades/spades-2.png")} alt="suit-of-spades" /> 
+    </div>
+    <div class="suit-spades">
+    <img class="suit-clubs-cards 3" src={require("../../assets/img/suit-of-spades/spades-3.png")} alt="suit-of-spades" /> 
+    </div>
+    <div class="suit-spades">
+    <img class="suit-clubs-cards 4" src={require("../../assets/img/suit-of-spades/spades-4.png")} alt="suit-of-spades" /> 
+    </div>
+    <div class="suit-spades">
+    <img class="suit-clubs-cards 5" src={require("../../assets/img/suit-of-spades/spades-5.png")} alt="suit-of-spades" /> 
+    </div>
+    <div class="suit-spades">
+    <img class="suit-clubs-cards 6" src={require("../../assets/img/suit-of-spades/spades-6.png")} alt="suit-of-spades" /> 
+    </div>
+    <div class="suit-spades">
+    <img class="suit-clubs-cards 7" src={require("../../assets/img/suit-of-spades/spades-7.png")} alt="suit-of-spades" /> 
+    </div>
+    <div class="suit-spades">
+    <img class="suit-clubs-cards 8" src={require("../../assets/img/suit-of-spades/spades-8.png")} alt="suit-of-spades" /> 
+    </div>
+    <div class="suit-spades">
+    <img class="suit-clubs-cards 9" src={require("../../assets/img/suit-of-spades/spades-9.png")} alt="suit-of-spades" /> 
+    </div>
+    <div class="suit-spades">
+    <img class="suit-clubs-cards 10" src={require("../../assets/img/suit-of-spades/spades-10.png")} alt="suit-of-spades" /> 
+    </div>
+    <div class="suit-spades">
+    <img class="suit-clubs-cards A" src={require("../../assets/img/suit-of-spades/spades-A.png")} alt="suit-of-spades" /> 
+    </div>
+    </div>
+    </main>
+    )
+}
+
+export default NotDealt;
