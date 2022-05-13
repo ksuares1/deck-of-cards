b2314adee457bb1ebd4dca9778ca63f983b7abb9
ksuares1 <kimberlyjsuares@gmail.com>
2022-05-13 01:41:10 -0400

    added suits to game

--- cards/src/component/Cards/Button.js
+++ cards/src/component/Cards/Button.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 class LoggingButton extends React.Component {
-    // This syntax ensures `this` is bound within handleClick.
-    // Warning: this is *experimental* syntax.
     handleClick = () => {
       console.log('this is:', this);
     }
--- cards/src/component/Cards/Cards.js
+++ cards/src/component/Cards/Cards.js
@@ -1,16 +1,21 @@
-import LoggingButton from "./Button.js";
+// import LoggingButton from "./Button.js";
 
 
 let Cards =() =>{
     return(
-        <div class="deck-game">
-        <div>
-        <img class="nd" src={require("../../assets/img/deck-pile.jpg")} alt="52-deck-of-cards"/>
-        <>
+        <div class="ND">
+        {/* ND- Cards */}
+        <div class="suit-clubs">
+            <img class="suit-clubs-2" src={require("../../assets/img/suit-of-clubs/Club-Suit-2.jpg")} alt="suit-of clubs" /> 
+        </div>
+        <div class="suit-clubs">
+            <img class="suit-clubs-2" src={require("../../assets/img/suit-of-clubs/Club-Suit-3.jpg")} alt="suit-of clubs" /> 
+        </div>
+        {/* <>
         <LoggingButton/>
-        </>
+        </> */}
     </div>
-    </div>
+    
     )
 }
 
