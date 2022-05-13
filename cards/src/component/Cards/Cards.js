db5592927efc2269e25e1ea7bf943593d51770b2
ksuares1 <kimberlyjsuares@gmail.com>
2022-05-13 02:14:48 -0400

    added hearts and css

--- cards/src/component/Cards/Cards.js
+++ cards/src/component/Cards/Cards.js
@@ -4,9 +4,9 @@
 let Cards =() =>{
     return(
         <main>
-        <div class="welcome">
+        <header class="welcome">
         <h1> Welcome to Festival Cards</h1>
-        </div>
+        </header>
         <br/>
         <>
         <LoggingButton/>
@@ -55,6 +55,52 @@ let Cards =() =>{
         </div>
         <div class="suit-clubs">
             <img class="suit-clubs-cards Q" src={require("../../assets/img/suit-of-clubs/Club-Suit-Queen.jpg")} alt="suit-of clubs" /> 
+        </div>
+        </div>
+
+        <br/>
+        <div class="ND suit-hearts">
+        <div class="suit-hearts">
+            <img class="suit-clubs-cards A" src={require("../../assets/img/suit-of-hearts/hearts-A.png")} alt="suit-of hearts" /> 
+        </div>
+        <div class="suit-hearts">
+            <img class="suit-clubs-cards 2" src={require("../../assets/img/suit-of-hearts/hearts-2.png")} alt="suit-of hearts" /> 
+        </div>
+        <div class="suit-hearts">
+            <img class="suit-clubs-cards 3" src={require("../../assets/img/suit-of-hearts/hearts-3.png")} alt="suit-of hearts" /> 
+        </div>
+        <div class="suit-hearts">
+            <img class="suit-clubs-cards 4" src={require("../../assets/img/suit-of-hearts/hearts-4.png")} alt="suit-of hearts" /> 
+        </div>
+        <div class="suit-hearts">
+            <img class="suit-clubs-cards 5" src={require("../../assets/img/suit-of-hearts/hearts-5.png")} alt="suit-of hearts" /> 
+        </div>
+        <div class="suit-hearts">
+            <img class="suit-clubs-cards 6" src={require("../../assets/img/suit-of-hearts/hearts-6.png")} alt="suit-of hearts" /> 
+        </div>
+        <div class="suit-hearts">
+            <img class="suit-clubs-cards 7" src={require("../../assets/img/suit-of-hearts/hearts-7.png")} alt="suit-of hearts" /> 
+        </div>
+        <div class="suit-hearts">
+            <img class="suit-clubs-cards 8" src={require("../../assets/img/suit-of-hearts/hearts-8.png")} alt="suit-of hearts" /> 
+        </div>
+        <div class="suit-hearts">
+            <img class="suit-clubs-cards 9" src={require("../../assets/img/suit-of-hearts/hearts-9.png")} alt="suit-of hearts" /> 
+        </div>
+        <div class="suit-hearts">
+            <img class="suit-clubs-cards 10" src={require("../../assets/img/suit-of-hearts/hearts-10.png")} alt="suit-of hearts" /> 
+        </div>
+        <div class="suit-hearts">
+            <img class="suit-clubs-cards A" src={require("../../assets/img/suit-of-hearts/hearts-A.png")} alt="suit-of hearts" /> 
+        </div>
+        <div class="suit-hearts">
+            <img class="suit-clubs-cards J" src={require("../../assets/img/suit-of-hearts/hearts-J.png")} alt="suit-of hearts" /> 
+        </div>
+        <div class="suit-hearts">
+            <img class="suit-clubs-cards K" src={require("../../assets/img/suit-of-hearts/hearts-K.png")} alt="suit-of hearts" /> 
+        </div>
+        <div class="suit-hearts">
+            <img class="suit-clubs-cards Q" src={require("../../assets/img/suit-of-hearts/hearts-Q.png")} alt="suit-of hearts" /> 
         </div>
         </div>
         </main>
